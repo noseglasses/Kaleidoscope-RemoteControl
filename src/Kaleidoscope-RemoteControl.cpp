37596d54eed9e241588618d041e86afbaf0bbacd
Florian Fleissner <florian.fleissner@inpartik.de>
2019-05-16 00:10:11 +0200

    Major redesign, generalization and simplification of the interface
    Signed-off-by: Florian Fleissner <florian.fleissner@inpartik.de>

--- src/Kaleidoscope-RemoteControl.cpp
+++ src/Kaleidoscope-RemoteControl.cpp
@@ -28,14 +28,14 @@ namespace remote_control {
 // void confirmReceive() {
 //    ::LEDControl.set_all_leds_to(CRGB(0, 255, 0));
 //    ::LEDControl.syncLeds();
-//    delay(1000);
+//    delay(500);
 //    ::LEDControl.set_all_leds_to(CRGB(0, 0, 0));
 //    ::LEDControl.syncLeds();
 // } 
 // void confirmSend() {
 //    ::LEDControl.set_all_leds_to(CRGB(0, 0, 255));
 //    ::LEDControl.syncLeds();
-//    delay(1000);
+//    delay(500);
 //    ::LEDControl.set_all_leds_to(CRGB(0, 0, 0));
 //    ::LEDControl.syncLeds();
 // }
@@ -43,7 +43,7 @@ namespace remote_control {
 void onFocusReceiveError() {
    ::LEDControl.set_all_leds_to(CRGB(255, 0, 0));
    ::LEDControl.syncLeds();
-   delay(1000);
+   delay(500);
    ::LEDControl.set_all_leds_to(CRGB(0, 0, 0));
    ::LEDControl.syncLeds();
 }
@@ -51,32 +51,23 @@ void onFocusReceiveError() {
 } // namespace remote_control
    
 EventHandlerResult RemoteControl::onFocusEvent(const char *command) {
+
+   static constexpr uint8_t cmd_length = 14;
    
-   if (::Focus.handleHelp(command, PSTR(
-      "remote_control.set <id> [value]\n"
-      "remote_control.get <id>\n"
-      "remote_control.trigger <id>")))
-    return EventHandlerResult::OK;
-   
-   static constexpr int cmd_length = 15;
-   
-   if (strncmp_P(command, PSTR("remote_control."), cmd_length) != 0)
+   if(strncmp_P(command, PSTR("remote_control"), cmd_length) != 0) {
       return EventHandlerResult::OK;
+   }
+   
+   if(   (command[cmd_length] != '\0') 
+      && (strcmp_P(command + cmd_length + 1, PSTR("help")) == 0)) {
+      remote_control::help();
+      return EventHandlerResult::OK;
+   }
    
    uint8_t id = 0;
    Focus.read(id);
   
-   if (strcmp_P(command + cmd_length, PSTR("set")) == 0) {
-      remote_control::receive(id);
-//       remote_control::confirmReceive();
-   }
-   else if (strcmp_P(command + cmd_length, PSTR("get")) == 0) {
-      remote_control::send(id);
-//       remote_control::confirmSend();
-   }
-   else if (strcmp_P(command + cmd_length, PSTR("trigger")) == 0) {
-      remote_control::trigger(id);
-   }
+   remote_control::process(id);
    
    return EventHandlerResult::OK;
 }
